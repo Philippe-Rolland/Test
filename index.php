fe3cdbc73974366ff44c6259d3d016f23c94e7a3
Philippe-Rolland <phetflo.rolland@orange.fr>
2017-04-02 16:43:23 +0200

    Squashed commit of the following:
    commit 2d665c7e7e150af2d0f8e7753f18c8edc44669d2
    Author: Philippe-Rolland <phetflo.rolland@orange.fr>
    Date:   Sun Apr 02 16:43:16 2017 +0200
    
    	Suppression des var_dump inutiles

--- index.php
+++ index.php
@@ -7,24 +7,15 @@ $merlin = new Personnage("Merlin");
 
 $harry = new Personnage("Harry");
 
-var_dump($merlin);
-var_dump($harry);
-
-
 $merlin->revivre();
 $merlin->crier();
 $merlin->mort();
 
-var_dump($merlin);
-var_dump($harry);
 
 echo "Bonjour " . $merlin->vie;
 
 $merlin->tuer();
 $merlin->mort();
-
-var_dump($merlin);
-var_dump($harry);
 
 $armee = new Armee("Gwen");
 
